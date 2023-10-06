e7c8fab0f452db6b6508c6dc199b19fbd039c607
Nezha <wwit.llt@gmail.com>
2023-10-05 18:01:54 -0700

    update api url

--- dashboard/src/config.tsx
+++ dashboard/src/config.tsx
@@ -1,2 +1,2 @@
-//export const API_URL = "https://eth-p2b-node.tech/api/rewards";
-export const API_URL = "http://localhost:3030/api/rewards";
+export const API_URL = "https://eth-p2b-node.tech/api/rewards";
+// export const API_URL = "http://localhost:3030/api/rewards";
